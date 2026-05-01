aff986e61a87740110dcb6a2caa4c51d8ba1d86f
pranshuritgithub <prashantjais004@gmail.com>
2026-05-01 14:34:02 +0530

    fix server.js for railway deployement

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,9 @@ app.use('/api/users', require('./routes/users'));
 
 app.get('/', (req, res) => res.json({ message: 'Team Task Manager API running' }));
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('DB connection error:', err));
